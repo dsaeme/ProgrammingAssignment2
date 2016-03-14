e7b1e74706d875ce024c5e0882219d1072ab981e
Dounia S <dsaeme@gmail.com>
2016-03-13 18:17:39 -0700

    corrected typos in cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## Assignment: Caching the ierse of a Matrix
+## Assignment: Caching the inverse of a Matrix
 ## This file contains a pair of functions that
-## cache the ierse of a matrix.
+## cache the inverse of a matrix.
 
 ## This function creates a special "matrix" object
-## that can cache its ierse.
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -20,10 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
              getInv = getInv)
 }
 
-## This function computes the ierse of the special
-## "matrix" returned by `makeCacheMatrix` above. If the ierse has
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
 ## already been calculated (and the matrix has not changed), then
-## `cacheSolve` should retrieve the ierse from the cache.
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
